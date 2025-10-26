d3756ddd300b9c6df845299e0d593c40fe845f8b
rhage <asya.makarska@gmail.com>
2025-10-26 21:49:19 +0300

    Beautified comments in fib.asm

--- fib.asm
+++ fib.asm
@@ -1,8 +1,8 @@
 start:
-    ADD x2, x0, x0  ; a = 0
-    SLTI x3, x0, #1  ; 1
-    ADD x4, x0, x3  ; i = 1
-    ADD x5, x3, x0  ; b = x5 = 1
+    ADD x2, x0, x0      ; a = 0
+    SLTI x3, x0, #1     ; 1
+    ADD x4, x0, x3      ; i = 1
+    ADD x5, x3, x0      ; b = x5 = 1
 
     ; if x1 == 0
     BEQ x1, x2, print_zero
